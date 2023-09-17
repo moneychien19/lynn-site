f3f278fe66da8e243d44ba6f3613e7f2382d6916
Lynn Chien <moneychien.aom@gmail.com>
2023-09-18 01:20:50 +0800

    feat: mdx code block config, new post astro-code-block

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+import astroExpressiveCode from "astro-expressive-code";
+
+const astroExpressiveCodeOptions = {
+  theme: "rose-pine-dawn",
+};
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()]
+  integrations: [
+    react(),
+    astroExpressiveCode(astroExpressiveCodeOptions),
+    mdx(),
+  ],
 });
