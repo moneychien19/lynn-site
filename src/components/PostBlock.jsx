662fbddd09e285029ad1ac103f4a6d19a32888c6
Lynn Chien <moneychien.aom@gmail.com>
2024-06-22 23:49:59 +0800

    feat: refine styles in homepage

--- src/components/PostBlock.jsx
+++ src/components/PostBlock.jsx
@@ -1,12 +1,23 @@
 import styled from "@emotion/styled";
 import { Card } from "@mui/material";
+import { Tag } from "./Tag"
 
-export const PostBlock = ({ title, content, url }) => {
+export const PostBlock = (props) => {
+  const { title, content, url, imageUrl, imageAlt, tags } = props
+
   return (
     <StyledA href={url}>
-      <StyledCard variant="outlined">
-        <StyledTitle>{title}</StyledTitle>
-        <StyledDescription>{content}</StyledDescription>
+      <StyledCard>
+        <StyledImage src={imageUrl} alt={imageAlt} />
+        <StyledTextContainer>
+          <StyledTitle>{title}</StyledTitle>
+          <StyledDescription>{content}</StyledDescription>
+          <StyledTagsContainer>
+            {tags.map(tag => (
+              <Tag label={tag} />
+            ))}
+          </StyledTagsContainer>
+        </StyledTextContainer>
       </StyledCard>
     </StyledA>
   );
@@ -19,18 +30,28 @@ const StyledCard = styled(Card)`
   width: 100%;
   padding: 0.5rem 2rem;
   box-sizing: border-box;
-  margin-bottom: 1rem;
-  &:hover {
-    box-shadow: 2px -2px 4px 0px #4a55a2;
-  }
-  &:active {
-    background-color: #f7f7f7;
+  &.MuiPaper-root {
+    box-shadow: none;
+    display: flex;
+    gap: 2rem;
   }
 `;
+const StyledImage = styled.img`
+  width: 20rem;
+`
+const StyledTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+`
 const StyledTitle = styled.h1`
   font-size: 1.75rem;
-  color: #4a55a2;
+  margin-bottom: 0.5rem;
 `;
 const StyledDescription = styled.p`
-  color: #262a56;
+  margin-top: 0.5rem;
 `;
+const StyledTagsContainer = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`
