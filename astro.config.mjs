bde70908e19db39b279617046d267434f5a6c3c5
lynn.chien <moneychien.aom@gmail.com>
2024-07-21 17:34:20 +0800

    feat: add ga base code in project

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,23 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import mdx from '@astrojs/mdx'
 import astroExpressiveCode from 'astro-expressive-code'
-
+import partytown from '@astrojs/partytown'
 const astroExpressiveCodeOptions = {
   theme: 'min-light',
 }
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), astroExpressiveCode(astroExpressiveCodeOptions), mdx()],
+  integrations: [
+    react(),
+    astroExpressiveCode(astroExpressiveCodeOptions),
+    mdx(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
   // workaround: fix build error
   // ref: https://github.com/withastro/astro/issues/7629
   vite: {
